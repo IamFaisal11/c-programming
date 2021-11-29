73b2301409749007ed7b4324633f042a9284202d
IamFaisal11 <94454129+IamFaisal11@users.noreply.github.com>
2021-11-29 01:25:59 -0800

    Update CALCULATOR.C

--- CALCULATOR.C
+++ CALCULATOR.C
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
-{    int x, y ,z, v; 
+{    int x, y , v;
+     float z; 
      printf("enter the integer");
      scanf("%d %d",&x,&y);
      printf("Enter 1 for addition, 2 for subtraction, 3 for multiplaction , 4 for division");
@@ -9,14 +10,14 @@ int main()
      {
                case 1:
              z=x+y;
-               printf("the sum of %d and %d is %d",x,y,z);
+               printf("the sum of %d and %d is %f",x,y,z);
                break;
            
      
      
              case 2:
              z=x-y;
-               printf("the subtraction of %d and %d is %d",x,y,z);
+               printf("the subtraction of %d and %d is %f",x,y,z);
                break;
                
                
@@ -25,14 +26,14 @@ int main()
       
             case 3:
              z=x*y;
-               printf("the product of %d and %d is %d",x,y,z);
+               printf("the product of %d and %d is %f",x,y,z);
                break;
                
                
                
                case 4:
                 z=x/y;
-               printf("the divion of %d and %d is %d",x,y,z);
+               printf("the divion of %d and %d is %f",x,y,z);
                break;
                
                defualt:
