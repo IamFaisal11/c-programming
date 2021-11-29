4f5f11bef6ce5dd2b09b353df04d7229a656c30a
IamFaisal11 <94454129+IamFaisal11@users.noreply.github.com>
2021-11-29 01:19:03 -0800

    Update CALCULATOR.C

--- CALCULATOR.C
+++ CALCULATOR.C
@@ -1,38 +1,46 @@
 #include<stdio.h>
 int main()
-{    int x, y, v; 
+{    int x, y ,z, v; 
      printf("enter the integer");
      scanf("%d %d",&x,&y);
      printf("Enter 1 for addition, 2 for subtraction, 3 for multiplaction , 4 for division");
      scanf("%d", &v);
-     if(v==1) {
-     v=x+y;
-     printf("answer = %d");}
-else if(v==2)
-{
-v=x-y;
-printf("answer = %d");
-      }
-  if(v==3)
-      {
-      v=x*y; printf("answer = %d");
-      }
- if(v==4)
-      {
-      v=x/y;
-      printf("answer = %d");
-      }
- else   
-      printf("invalid option");
-    
-      }
+     switch(v)
+     {
+               case 1:
+             z=x+y;
+               printf("the sum of %d and %d is %d",x,y,z);
+               break;
+           
+     
+     
+             case 2:
+             z=x-y;
+               printf("the subtraction of %d and %d is %d",x,y,z);
+               break;
+               
+               
+               
       
       
+            case 3:
+             z=x*y;
+               printf("the product of %d and %d is %d",x,y,z);
+               break;
+               
+               
+               
+               case 4:
+                z=x/y;
+               printf("the divion of %d and %d is %d",x,y,z);
+               break;
+               
+               defualt:
+               printf("invaild option");
       
       
-      
-      
-      
+     }
+}
       
       
       
