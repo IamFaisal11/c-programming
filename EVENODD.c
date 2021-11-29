a4ebc96e847a00c115a7e60c50a0ae15e9387d6e
IamFaisal11 <94454129+IamFaisal11@users.noreply.github.com>
2021-11-29 00:50:25 -0800

    Add files via upload

---
+++ EVENODD.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int num;
+clrscr();
+printf("please enter first number");
+scanf("%d", &num);
+if(num%2==0)
+	printf("the number is even");
+else
+	printf("the number is odd");
+	getche();
+}
