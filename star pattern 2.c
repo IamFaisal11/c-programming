f170ef42bf17ee5ca0afa2478d7b5a46855cafeb
IamFaisal11 <94454129+IamFaisal11@users.noreply.github.com>
2021-11-30 05:51:00 -0800

    Create star pattern 2.c

---
+++ star pattern 2.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j, n;
+    for(i=0;i<=5;i++)
+    {
+        for(j=5;j<=0;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
